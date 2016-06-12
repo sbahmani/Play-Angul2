0d021c0a265de43057832f4e802747bcc3f5f7c3
Sajad Bahmani <bahmani.s@gmail.com>
2016-06-12 16:34:28 +0430

    Multiple Components

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,10 +1,6 @@
 import {Component} from "@angular/core";
-
-export class Hero {
-    id:number;
-    name:string;
-}
-
+import {Hero} from "./hero";
+import {HeroDetailComponent} from "./hero-detail.component";
 @Component({
     selector: 'my-app',
     template: `
@@ -17,14 +13,7 @@ export class Hero {
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
     styles: [`
     .selected {
@@ -74,7 +63,8 @@ export class Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+    directives: [HeroDetailComponent]
 })
 export class AppComponent {
     title = 'Tour of Heroes';
@@ -85,7 +75,6 @@ export class AppComponent {
         this.selectedHero = hero;
     }
 }
-
 var HEROES:Hero[] = [
     {"id": 11, "name": "Mr. Nice"},
     {"id": 12, "name": "Narco"},
@@ -98,10 +87,3 @@ var HEROES:Hero[] = [
     {"id": 19, "name": "Magma"},
     {"id": 20, "name": "Tornado"}
 ];
-
-
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license
- */
---
+++ public/app/hero-detail.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by sjb on 12/06/16.
+ */
+import {Component, Input} from "@angular/core";
+import {Hero} from "./hero";
+@Component({
+    selector: 'my-hero-detail',
+    template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+export class HeroDetailComponent {
+    @Input()
+    hero:Hero;
+}
