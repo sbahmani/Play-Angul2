537ffcddc2d8e97df77ef927054b802fedb82561
Sajad Bahmani <bahmani.s@gmail.com>
2016-06-12 17:29:03 +0430

    Multiple Components

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -3,18 +3,8 @@ import {Hero} from "./hero";
 import {HeroDetailComponent} from "./hero-detail.component";
 @Component({
     selector: 'my-app',
-    template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `,
+    templateUrl: 'a.html',
+    moduleId: module.id,
     styles: [`
     .selected {
       background-color: #CFD8DC !important;
