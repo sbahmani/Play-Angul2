4ac993ded6d53ee292d6e6130b6341332d1c256a
Sajad Bahmani <bahmani.s@gmail.com>
2016-06-13 16:40:11 +0430

    Service Provider

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Hero} from "./hero";
 import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
 @Component({
     selector: 'my-app',
     templateUrl: 'a.html',
@@ -54,26 +55,27 @@ import {HeroDetailComponent} from "./hero-detail.component";
       border-radius: 4px 0 0 4px;
     }
   `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes:Hero[];
     selectedHero:Hero;
+
+    constructor(private heroService:HeroService) {
+    }
+
+    getHeroes() {
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
 
     onSelect(hero:Hero) {
         this.selectedHero = hero;
     }
 }
-var HEROES:Hero[] = [
-    {"id": 11, "name": "Mr. Nice"},
-    {"id": 12, "name": "Narco"},
-    {"id": 13, "name": "Bombasto"},
-    {"id": 14, "name": "Celeritas"},
-    {"id": 15, "name": "Magneta"},
-    {"id": 16, "name": "RubberMan"},
-    {"id": 17, "name": "Dynama"},
-    {"id": 18, "name": "Dr IQ"},
-    {"id": 19, "name": "Magma"},
-    {"id": 20, "name": "Tornado"}
-];
