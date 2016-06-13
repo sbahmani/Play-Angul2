4df614fd27b851b0d5adcd39f8cbb82ea4391043
Sajad Bahmani <bahmani.s@gmail.com>
2016-06-13 16:41:53 +0430

    Service Provider

---
+++ public/app/hero.service.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by sjb on 13/06/16.
+ */
+import {Injectable} from "@angular/core";
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+
+    // See the "Take it slow" appendix
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+        );
+    }
+}
