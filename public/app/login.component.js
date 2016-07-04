7d43c2c7f2723bec1bcaa518d6e594f9a057afa9
Sajad Bahmani <bahmani.s@gmail.com>
2016-07-04 19:30:28 +0430

    restrict in angular for authentication

---
+++ public/app/login.component.js
@@ -0,0 +1,51 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+/**
+ * Created by sjb on 04/07/16.
+ */
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var auth_service_1 = require("./auth.service");
+var LoginComponent = (function () {
+    function LoginComponent(authService, router) {
+        this.authService = authService;
+        this.router = router;
+        this.setMessage();
+    }
+    LoginComponent.prototype.setMessage = function () {
+        this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+    };
+    LoginComponent.prototype.login = function () {
+        var _this = this;
+        this.message = 'Trying to log in ...';
+        this.authService.login().subscribe(function () {
+            _this.setMessage();
+            if (_this.authService.isLoggedIn) {
+                // Todo: capture where the user was going and nav there.
+                // Meanwhile redirect the user to the crisis admin
+                _this.router.navigate(['/dashboard']);
+            }
+        });
+    };
+    LoginComponent.prototype.logout = function () {
+        this.authService.logout();
+        this.setMessage();
+    };
+    LoginComponent = __decorate([
+        core_1.Component({
+            template: "\n    <h2>LOGIN</h2>\n    <p>{{message}}</p>\n    <p>\n      <button (click)=\"login()\"  *ngIf=\"!authService.isLoggedIn\">Login</button>\n      <button (click)=\"logout()\" *ngIf=\"authService.isLoggedIn\">Logout</button>\n    </p>"
+        }), 
+        __metadata('design:paramtypes', [auth_service_1.AuthService, router_1.Router])
+    ], LoginComponent);
+    return LoginComponent;
+}());
+exports.LoginComponent = LoginComponent;
+//# sourceMappingURL=login.component.js.map
